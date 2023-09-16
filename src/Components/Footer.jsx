c086860dbde65c9543b5cd79d53dd10828a1b33a
Martin Acosta <martin.aco@hotmail.com>
2023-09-15 21:42:44 -0300

    Final

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import img from '../assets/images/Dh.png'
+
 
 
 const Footer = () => {
   return (
     <footer>
         <p>Powered by</p>
-        <img src={img} alt='DH-logo' />
+        <img src = "src/assets/images/DH.png" alt='DH-logo' />
             </footer>
   )
 }
